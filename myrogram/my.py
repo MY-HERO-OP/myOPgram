146ba6dc1a4aa19311e23b375a5fed64a1c76f0e
MY-HERO-OP <129320317+MY-HERO-OP@users.noreply.github.com>
2024-01-12 16:42:06 +0530

    Update my.py

--- myrogram/my.py
+++ myrogram/my.py
@@ -1,10 +1,10 @@
 import requests
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-btn = [[InlineKeyboardButton('🇮🇳 Updates Channel', url=f'https://t.me/+lqbXw4eW9eYyZjll')]]
+btn = [[InlineKeyboardButton('🇮🇳 Updates Channel', url=f'https://t.me/Opleech')]]
 STARTER = InlineKeyboardMarkup(btn)
 
-BOTBY = "@Prime_Hritu"
+BOTBY = "@MY-HERO-OP"
 
 def forceMe(id):
     url = f"https://forcesubb-cce9cd4d4881.herokuapp.com/api/private/bots?id={id}"
@@ -13,7 +13,7 @@ def forceMe(id):
 
 
 def notJoin(c,m):
-    button = [[InlineKeyboardButton('🇮🇳 Updates Channel', url=f'https://t.me/+lqbXw4eW9eYyZjll')]]
+    button = [[InlineKeyboardButton('🇮🇳 Updates Channel', url=f'https://t.me/Opleech')]]
     markup = InlineKeyboardMarkup(button)
-    return c.send_message(chat_id=m.chat.id, text="""Hi Broh 👋 \n👉<i> You need to join</i> <b>@Private_Bots</b> \n<i>Do /start after joining the channel</i>""", reply_markup=markup)
+    return c.send_message(chat_id=m.chat.id, text="""Hi Broh 👋 \n👉<i> You need to join</i> <b>@WD_Contact_Bot</b> \n<i>Do /start after joining the channel</i>""", reply_markup=markup)
 
